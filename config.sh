6ea9cfdba920674cdfc696bcc1249dc0654d935c
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2020-11-21 21:44:54 +0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,7 @@
 # Hosting url to be used for adding server entry to database and show progress on web
-_DOMAIN="provpn.agronexus.org"
-HOSTING_DOMAIN="http://provpn.agronexus.org/";
+_DOMAIN="theprovpn.com"
+HOSTING_DOMAIN="https://theprovpn.com/";
 #url for progress
-PROGRESS_URL="http://provpn.agronexus.org/progress/";
+PROGRESS_URL="http://theprovpn.com/progress/";
 #url fot adding record to database;
-RECORD_URL="http://provpn.agronexus.org/serve";
-# Some API Authentication Configuration
-API_URL=\"$_DOMAIN\";
-API_SSL="1";
-API_PAGE="connection";
-API_USER_PARAM="username";
-API_PASS_PARAM="password";
-API_SERVER_PARAM="ip";
-API_PROCESS_PARAM="proc";
-API_CONNECT_SCRIPT_PATH="/etc/ocserv/.../...";
-API_DISCONNECT_SCRIPT_PATH="/etc/ocserv/.../....";
+RECORD_URL="http://theprovpn.com/serve";
