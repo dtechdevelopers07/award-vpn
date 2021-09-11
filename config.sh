09da1faea36d4c71a791ca9f78ba8baa1b7b7aba
dtechdevelopers07 <55192607+dtechdevelopers07@users.noreply.github.com>
2021-09-12 01:12:32 +0500

    Domain Update

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # Hosting url to be used for adding server entry to database and show progress on web
-_DOMAIN="theprovpn.com"
-HOSTING_DOMAIN="https://theprovpn.com/";
+_DOMAIN="award-vpn.com"
+HOSTING_DOMAIN="https://award-vpn.com/";
 #url for progress
-PROGRESS_URL="https://theprovpn.com/progress/";
+PROGRESS_URL="https://award-vpn.com/progress/";
 #url fot adding record to database;
-RECORD_URL="https://theprovpn.com/serve";
+RECORD_URL="https://award-vpn.com/serve";
